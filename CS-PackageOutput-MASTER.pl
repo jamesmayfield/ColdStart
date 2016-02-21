84732388f4ebef7c12ec3dd8bdf5ce69fa5fc060
Shahzad Rajput <shahzad.rajput@gmail.com>
2016-02-21 15:25:17 -0500

    CS-PackageOutput-MASTER.pl version upped to reflect changed library

--- CS-PackageOutput-MASTER.pl
+++ CS-PackageOutput-MASTER.pl
@@ -23,7 +23,7 @@ use ColdStartLib;
 # For usage, run with no arguments
 ##################################################################################### 
 
-my $version = "1.4";
+my $version = "2.0";
 
 # Filehandles for program and error output
 my $program_output = *STDOUT{IO};
@@ -137,4 +137,5 @@ exit 0;
 # 1.2 - Bug fixes
 # 1.3 - Handle 2015 format changes
 # 1.4 - Further 2015 format changes
+# 2.0 - Version upped to reflect changes in the library
 1;
