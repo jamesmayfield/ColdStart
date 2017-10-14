725885cf237825dc786db257f152df4006f5dec2
Shahzad Rajput <shahzad.rajput@gmail.com>
2017-10-14 15:07:57 -0400

    Crash with a proper message is mention_type is not defined

--- ColdStartLib.pm
+++ ColdStartLib.pm
@@ -2366,6 +2366,9 @@ sub score_subtree {
         my $value_mention_type = $submission->{ASSESSMENT}{VALUE_MENTION_TYPE};
         my $ec_mention_type = $submission->{ASSESSMENT}{EC_MENTION_TYPE};
 
+        $self->{LOGGER}->NIST_die("Missing VALUE_MENTION_TYPE for submission entry: $submission->{LINE}\n") unless $value_mention_type;
+        $self->{LOGGER}->NIST_die("Missing EC_MENTION_TYPE for submission entry: $submission->{LINE}\n") unless $ec_mention_type;
+
         $ignore{$fqnodeid} = 0 if ($value_mention_type eq "NAM" || $ec_mention_type eq "NOM");
       }
       last if $i==$k;
